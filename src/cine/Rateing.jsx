b5972a2eba2dcc9ae5d4092b8c22995ea2ede3ac
sejanmiah <sejankhan931@gmail.com>
2025-08-06 01:13:17 +0400

    Dynamic Rateing Done

---
+++ src/cine/Rateing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Star from "../assets/star.svg"
+
+const Rateing = ({value}) => {
+    const stars = Array(value).fill(Star)
+    console.log(stars);
+    return (
+        <>
+            {
+                stars.map((star,index) => (
+                    <img key={index} width="14" alt='star' height="14"  src={star}  />
+                ))
+            }
+        </>
+    );
+};
+
+export default Rateing;
